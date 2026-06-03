a843046a1a98fe655460d10829ec909c0975c7a1
Kochab-02 <wzy1379@Outlook.com>
2026-06-03 15:16:44 +0800

    1.5.1-1.20.1: added a function to help calculate distance.

--- MoreThanYieldingAndCreating/src/main/java/com/kochab02/moreThanYAC/utils/MathUtils.java
+++ MoreThanYieldingAndCreating/src/main/java/com/kochab02/moreThanYAC/utils/MathUtils.java
@@ -1,5 +1,8 @@
 package com.kochab02.moreThanYAC.utils;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.LivingEntity;
+
 public class MathUtils
 {
     public static int sgn(int a){
@@ -10,5 +13,16 @@ public class MathUtils
     }
     public static double sgn(double a){
         return Double.compare(a, 0);
+    }
+
+    public static double distanceTo(LivingEntity entity, BlockPos pos){
+        double x1 = entity.getX();
+        double x = pos.getX();
+        double y1 = entity.getY();
+        double y = pos.getY();
+        double z1 = entity.getZ();
+        double z = pos.getZ();
+
+        return Math.sqrt(Math.pow(x1-x,2)+Math.pow(y1-y,2)+Math.pow(z1-z,2));
     }
 }
